1538fbd33307db6646d57bd6e04c1504f884cbe2
Vincent Schänzer <schaenzer@modell-aachen.de>
2022-10-26 14:48:10 +0000

    Add logging handler for invalid requests

--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
+import structlog
 from fastapi import APIRouter, FastAPI
+from fastapi.exception_handlers import request_validation_exception_handler
+from fastapi.exceptions import RequestValidationError
 
 from app import __version__
 from app.endpoints import healthz, mutate
+
+log = structlog.get_logger(__name__)
 
 app = FastAPI(
     title='Mutating Webhook no CPU limit',
@@ -14,3 +19,8 @@ api_router.include_router(healthz.router, prefix='/healthz', tags=['healthz'])
 api_router.include_router(mutate.router, prefix='/mutate', tags=['mutate'])
 
 app.include_router(api_router)
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request, exc):
+    log.error('invalid_data_received', details=exc.errors())
+    return await request_validation_exception_handler(request, exc)
